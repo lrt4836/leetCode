a077cd9ead50251cbdb605f4555bd4df07e28564
lrt4836 <296659110@qq.com>
2019-11-14 17:43:18 +0800

    83: 链表去重

---
+++ leetcode/editor/cn/[83]删除排序链表中的重复元素.js
@@ -0,0 +1,40 @@
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//
+// 示例 1: 
+//
+// 输入: 1->1->2
+//输出: 1->2
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3 
+// Related Topics 链表
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  let current = head
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next
+    } else {
+      current = current.next
+    }
+  }
+  return head
+};
+//leetcode submit region end(Prohibit modification and deletion)
