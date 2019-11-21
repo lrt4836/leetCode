39744dba06332737a9017b34f00497719e58eb5c
linruitao <linruitao@zhuifintech.com>
2019-11-21 15:51:55 +0800

    107: 递归解决二叉树的层次遍历

---
+++ leetcode/editor/cn/[107]二叉树的层次遍历 II.js
@@ -0,0 +1,50 @@
+//给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层次遍历为： 
+//
+// [
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  let res = []
+  function push (node, i) {
+    if (node) {
+      if (!res[i]) res[i] = []
+      res[i].push(node.val)
+      push(node.left, i+1)
+      push(node.right, i+1)
+    }
+  }
+  push(root, 0)
+  return res.reverse()
+};
+//leetcode submit region end(Prohibit modification and deletion)
