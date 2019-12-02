d48ffecdaa7331f945e08ffcacfb65011350ae34
lrt4836 <296659110@qq.com>
2019-12-02 15:04:20 +0800

    5: 中心扩展求解最长回文子串

---
+++ leetcode/editor/cn/[5]最长回文子串.js
@@ -0,0 +1,47 @@
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。 
+//
+// 示例 1： 
+//
+// 输入: "babad"
+//输出: "bab"
+//注意: "aba" 也是一个有效答案。
+// 
+//
+// 示例 2： 
+//
+// 输入: "cbbd"
+//输出: "bb"
+// 
+// Related Topics 字符串 动态规划
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s === null || s.length < 1) return ''
+  let start = 0, end = 0;
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandAroundCenter(s, i, i);
+    let len2 = expandAroundCenter(s, i, i + 1);
+    let len = Math.max(len1, len2);
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+  return s.substring(start, end + 1);
+}
+
+function expandAroundCenter(s, left, right) {
+  let L = left, R = right;
+  while (L >= 0 && R < s.length && s[L] === s[R]) {
+    L--;
+    R++;
+  }
+  return R - L - 1;
+}
+//leetcode submit region end(Prohibit modification and deletion)
