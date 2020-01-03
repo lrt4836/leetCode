08259bb97d320dcbf2da204b72f844151170b866
lrt4836 <296659110@qq.com>
2020-01-03 10:43:05 +0800

    17: 回溯解决电话号码的字母组合

---
+++ leetcode/editor/cn/[17]电话号码的字母组合.js
@@ -0,0 +1,55 @@
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。 
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。 
+//
+// 
+//
+// 示例: 
+//
+// 输入："23"
+//输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+// 
+//
+// 说明: 
+//尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。 
+// Related Topics 字符串 回溯算法
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  // 边界处理
+  if (digits.length === 0) return []
+
+  let map = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+  }
+  let res = []
+
+  function backtrack (combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      res.push(combination)
+      return
+    }
+    let letters = map[nextDigits[0]]
+    for (let i = 0; i < letters.length; i++) {
+      let letter = letters[i]
+      backtrack(combination+letter, nextDigits.substring(1))
+    }
+  }
+
+  backtrack('', digits)
+  return res
+};
+//leetcode submit region end(Prohibit modification and deletion)
