86ca56eaeb7acbdf85a0f78afa57bb44fb9c288f
lrt4836 <296659110@qq.com>
2020-01-06 16:38:15 +0800

    19: 双指针删除链表倒数第k个节点

---
+++ leetcode/editor/cn/[19]删除链表的倒数第N个节点.js
@@ -0,0 +1,49 @@
+//给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。 
+//
+// 示例： 
+//
+// 给定一个链表: 1->2->3->4->5, 和 n = 2.
+//
+//当删除了倒数第二个节点后，链表变为 1->2->3->5.
+// 
+//
+// 说明： 
+//
+// 给定的 n 保证是有效的。 
+//
+// 进阶： 
+//
+// 你能尝试使用一趟扫描实现吗？ 
+// Related Topics 链表 双指针
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  let pre = new ListNode(0)
+  pre.next = head
+  let start = pre, end = pre
+  while (n) {
+    start = start.next
+    n--
+  }
+  while (start.next) {
+    start = start.next
+    end = end.next
+  }
+  end.next = end.next.next
+  return pre.next
+};
+//leetcode submit region end(Prohibit modification and deletion)
